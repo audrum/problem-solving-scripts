3db2caa3c88f4a1989dbf86496110357004d4792
Andres Bolivar <andresmaster1@gmail.com>
2022-05-30 16:01:01 +0400

    Some HackerRank solutions

---
+++ Python/HackerRank/text_wrap.py
@@ -0,0 +1,12 @@
+# Prepare > Python > Strings > Text Wrap
+
+import textwrap
+
+def wrap(string, max_width):
+    result = 
+    return
+
+if __name__ == '__main__':
+    string, max_width = input(), int(input())
+    result = wrap(string, max_width)
+    print(result)
