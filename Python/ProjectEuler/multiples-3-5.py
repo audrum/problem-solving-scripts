5dbef1d81d757a2aa34c37cf1a0802b07123a994
Andres Bolivar <andresmaster1@gmail.com>
2022-12-05 20:51:11 +0400

    Adding spacing

--- Python/ProjectEuler/multiples-3-5.py
+++ Python/ProjectEuler/multiples-3-5.py
@@ -1,8 +1,10 @@
 num = 0
 result = 0
+
 while num <= 100:
     if (num % 3 == 0) or (num % 5 == 0):
         print(num)
     result += num
     num += 1
+
 print(f"The sum of all multiples is {result}")
